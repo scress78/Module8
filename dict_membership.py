31cf52bc40671463c821cf876dc694e3c9d182a3
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-22 16:50:18 -0500

    Add files via upload

--- dict_membership.py
+++ dict_membership.py
@@ -9,17 +9,18 @@ as output.  The function is then called.
 """
 
 
-def in_dict():
+def in_dict(a_dict, a_key):
+    """Use reST style.
+    :param a_dict: a dictionary to be entered into the function
+    :param a_key: a key item to search for in the dictionary
+    :returns: True if item key is in the dictionary and false if not
     """
-    Use reST style.
+    return a_key in a_dict
 
-    :param parameter_1: this is what the first parameter represents
-    :param parameter_2: this is what the second parameter represents
-    :returns: this is what is returned
-    :raises keyError: raises an exception
-    """
-    pass
 
+first_dict = {'Apples': 90, 'Bananas': 100, 'Cherries': 300}
+first_search = 'Apples'
+# print(first_search in first_dict)
 
 if __name__ == '__main__':
-    hello_world()
+    in_dict(first_dict, first_search)
