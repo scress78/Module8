a156019aa0d98f889102f3be7a3e55de133477e4
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-23 13:11:46 -0500

    Add files via upload

--- datetime_assignment.py
+++ datetime_assignment.py
@@ -5,33 +5,35 @@ Date: 06/23/2020
 
 Datetime Assignment
 """
-from datetime import datetime, timedelta
+import datetime
+from datetime import timedelta
 
 #current = input("Input birthday in format YYYY-MM-DD")
 #print(current.datetime())
 
-graduation_date = datetime.datetime(2020, 5, 17)
-print(graduation_date)
+#graduation_date = datetime.datetime(2020, 5, 17)
+#print(graduation_date)
 
 
 def half_birthday(birthyear, birthmonth, birthday):
     year = int(birthyear)
     month = int(birthmonth)
     day = int(birthday)
-    birthday = datetime.(year, month, day)
-    print(birthday)
-    #birthday_half = birthday + timedelta(days=182)
+    birthday = datetime.datetime(year, month, day)
+    #print(birthday)
+    birthday_half = birthday + timedelta(days=182)
     #print(birthday_half)
     """
     Use reST style.
 
-    :param parameter_1: this is what the first parameter represents
-    :param parameter_2: this is what the second parameter represents
-    :returns: this is what is returned
-    :raises keyError: raises an exception
+    :param birthyear: year of the birthday in YYYY format
+    :param birthmonth: month of the birthday in MM format
+    :param birthday: day of the birthday in DD format
+    :returns: a date 6 months (182 days) after the date (birthday) entered
+    representing a 'half birthday'
     """
-    pass
+    return str(birthday_half)
 
 
 if __name__ == '__main__':
-   #half_birthday(2020, 5, 17)
+    half_birthday(2020, 8, 15)
--- test_datetime_assignment.py
+++ test_datetime_assignment.py
@@ -13,7 +13,8 @@ from more_fun_with_collections.datetime_assignment import half_birthday as hb
 class MyTestCase(unittest.TestCase):
 
     def test_half_birthday(self):
-        self.assertEqual(hb(2019, 8, 15), (2020, 2, 15))
+        """Tests half_birthday function for accuracy"""
+        self.assertEqual(hb(2019, 8, 15), '2020-02-13 00:00:00')
 
 
 if __name__ == '__main__':
