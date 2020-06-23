7ed822c45e8dfc5d26c69290c0fad410d1e4c093
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-22 19:37:54 -0500

    Add files via upload

---
+++ assign_average.py
@@ -0,0 +1,36 @@
+"""
+Program: assign_average.py
+Author:  Spencer Cress
+Date: 06/22/2020
+
+
+"""
+"""average = 0
+switch(average){
+    case 'A': ...statements...
+         break
+    case B: ...statements...
+         break
+    case s: ...statements...
+         break
+    case e: ...statements...
+         break
+    default: ...statements...
+        break
+ }"""
+
+
+def assign_average():
+    """
+    Use reST style.
+
+    :param parameter_1: this is what the first parameter represents
+    :param parameter_2: this is what the second parameter represents
+    :returns: this is what is returned
+    :raises keyError: raises an exception
+    """
+    pass
+
+
+if __name__ == '__main__':
+    hello_world()
---
+++ test_assign_average.py
@@ -0,0 +1,36 @@
+"""
+Program: test_assign_average.py
+Author:  Spencer Cress
+Date: 06/22/2020
+
+Tests assign_average functions
+"""
+import unittest
+import unittest.mock as mock
+from more_fun_with_collections.assign_average import assign_average as aa
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_assert_average_A(self):
+        self.assertEqual(aa(100), 'A')
+
+    def test_assert_average_B(self):
+        self.assertEqual(aa(85), 'B')
+
+    def test_assert_average_C(self):
+        self.assertEqual(aa(71), 'C')
+
+    def test_assert_average_D(self):
+        self.assertEqual(aa(64), 'D')
+
+    def test_assert_average_F(self):
+        self.assertEqual(aa(13), 'F')
+
+    def test_assert_average_Invalid(self):
+        self.assertRaises(aa('Taco'), ValueError)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
